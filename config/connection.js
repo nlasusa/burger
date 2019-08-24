44ff4c5265b92523045591ea76dd81e2263973ad
Nicole LaSusa <nicolelasusa@gmail.com>
2019-08-24 15:07:25 -0400

    herokutry

--- config/connection.js
+++ config/connection.js
@@ -9,20 +9,6 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDBURL); 
-// } else {
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "classy43",
-//   database: "burgers_db"
-// });
-// }; 
-
-
 connection.connect(); 
 
  // Export connection for our ORM to use.
