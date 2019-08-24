e9d408365e0d6052e40f3e9dab465dbc96758659
Nicole LaSusa <nicolelasusa@gmail.com>
2019-08-24 15:04:37 -0400

    herokutry

--- config/connection.js
+++ config/connection.js
@@ -9,16 +9,19 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost", 
-    user: "root", 
-    passsord: "classy43",
-    database: "burgers_db"
-  });
-};
+
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDBURL); 
+// } else {
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "classy43",
+//   database: "burgers_db"
+// });
+// }; 
+
 
 connection.connect(); 
 
